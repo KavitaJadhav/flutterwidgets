bcd07287e2af5a43ba69fc0c3200d79b048a4a25
Kavita Jadhav <jadhavkavita206@gmail.com>
2021-03-23 07:02:36 +0530

    Added datePicker.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(new MaterialApp(
@@ -13,18 +14,32 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State {
-  int _value = 0;
+  int _counter = 0;
+  String _date = '';
 
   void _add() {
     setState(() {
-      _value++;
+      _counter++;
     });
   }
 
   void _substract() {
     setState(() {
-      _value--;
+      _counter--;
     });
+  }
+
+  Future _selectDate() async {
+    DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: new DateTime(2016),
+        lastDate: new DateTime(2025));
+    if (picked != null)
+      setState(() {
+        _date = picked.toString();
+      });
+    ;
   }
 
   @override
@@ -38,10 +53,13 @@ class _State extends State {
         child: new Center(
           child: new Column(
             children: <Widget>[
-              new Text('value: ${_value}'),
+              new Text('value: ${_counter}'),
               new IconButton(icon: new Icon(Icons.add), onPressed: _add),
               new IconButton(
-                  icon: new Icon(Icons.remove), onPressed: _substract)
+                  icon: new Icon(Icons.remove), onPressed: _substract),
+              new Text('Date Selected: ${_date}'),
+              new RaisedButton(
+                  onPressed: _selectDate, child: new Text('Select Date')),
             ],
           ),
         ),
